2432414dd1efd2137a9d2b56307ed3e77dde314f
Lucas Bitencourt <lblukz@aol.com>
2022-06-29 15:06:43 -0300

    Requisito 1 concluído
    Básico para criação de requisito, sem redux e sem store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './trivia.png';
 import './App.css';
+import Login from './pages/Login';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <img src={ logo } className="App-logo" alt="logo" />
         <p>SUA VEZ</p>
       </header>
+      <Login />
     </div>
   );
 }
