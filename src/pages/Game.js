6b4823e4049d461da143df321dbb4d1dcbf92b4d
Lucas Bitencourt <lblukz@aol.com>
2022-06-30 14:54:48 -0300

    Início req 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,28 +6,46 @@ import { connect } from 'react-redux';
 class Game extends Component {
   state = {
     emailString: '',
+    questions: [],
   }
 
   componentDidMount() {
     const { email } = this.props;
     const emailString = md5(email).toString();
     this.setState({ emailString });
+    this.getQuestions();
+  }
+
+  getQuestions = async () => {
+    const { token } = this.props;
+    const numQuestions = 5;
+    const url = `https://opentdb.com/api.php?amount=${numQuestions}&token=${token}`;
+    const response = await fetch(url);
+    const questions = await response.json();
+    console.log(questions.results);
+    this.setState({ questions: questions.results });
   }
 
   render() {
-    const { emailString } = this.state;
+    const { emailString, questions } = this.state;
     const { name } = this.props;
     const gravatar = 'https://www.gravatar.com/avatar/';
     return (
-      <header className="game-header">
-        <img
-          src={ `${gravatar}${emailString}` }
-          alt="imagem jogador"
-          data-testid="header-profile-picture"
-        />
-        <span data-testid="header-player-name">{name}</span>
-        <span data-testid="header-score">0</span>
-      </header>
+      <>
+        <header className="game-header">
+          <img
+            src={ `${gravatar}${emailString}` }
+            alt="imagem jogador"
+            data-testid="header-profile-picture"
+          />
+          <span data-testid="header-player-name">{name}</span>
+          <span data-testid="header-score">0</span>
+        </header>
+        <main>
+          <h1>Categoria</h1>
+        </main>
+      </>
+
     );
   }
 }
@@ -35,6 +53,7 @@ class Game extends Component {
 const mapStateToProps = (state) => ({
   name: state.triviaReducer.name,
   email: state.triviaReducer.email,
+  token: state.triviaReducer.token,
 });
 
 // const mapDispatchToProps = {}
@@ -42,5 +61,6 @@ const mapStateToProps = (state) => ({
 Game.propTypes = {
   name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
+  token: PropTypes.string.isRequired,
 };
 export default connect(mapStateToProps)(Game);
