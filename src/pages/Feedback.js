0a964d7988aaf03325a57948b9f6c26cc3ae9951
Lucas Bitencourt <lblukz@aol.com>
2022-07-01 14:33:46 -0300

    Requisito 12 concluído

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   // state = {  }
   render() {
     return (
       <div data-testid="feedback-text">
+        <Header />
         <h1>Feedback</h1>
       </div>
     );
