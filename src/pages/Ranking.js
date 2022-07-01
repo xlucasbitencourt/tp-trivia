378686eab2ba1b521394896d45054c156bf441fe
Lucas Bitencourt <lblukz@aol.com>
2022-07-01 18:30:24 -0300

    Requisito 18 concluído

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   // state = {  }
   render() {
     return (
-      <p data-testid="ranking-title">Ranking</p>
+      <div className="ranking">
+        <p data-testid="ranking-title">Ranking</p>
+        <Link to="/" data-testid="btn-go-home">Play Again</Link>
+      </div>
     );
   }
 }
