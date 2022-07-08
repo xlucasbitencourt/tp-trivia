53ecfc04ae69cc0c5a93d7cd970605a7654a903a
Lucas Bitencourt <lblukz@aol.com>
2022-07-08 12:00:03 -0300

    Updates

---
+++ precache-manifest.c37c2cab20f3d2f8438807c4db3a6386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264fcd7378bf888c4344092edbb8550b",
+    "url": "/tp-trivia/index.html"
+  },
+  {
+    "revision": "782edb722c1b6d295f89",
+    "url": "/tp-trivia/static/css/main.b42d38af.chunk.css"
+  },
+  {
+    "revision": "a3155b44701b9a1b9a15",
+    "url": "/tp-trivia/static/js/2.3ae6abd1.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/tp-trivia/static/js/2.3ae6abd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782edb722c1b6d295f89",
+    "url": "/tp-trivia/static/js/main.77f5352c.chunk.js"
+  },
+  {
+    "revision": "cbe2f7b07346d50b013c",
+    "url": "/tp-trivia/static/js/runtime-main.730c5daa.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/tp-trivia/static/media/trivia.466d153e.png"
+  }
+]);
