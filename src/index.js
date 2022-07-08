da1d3367dc3a900f903cdfc3d86be74b785d114b
Lucas Bitencourt <lblukz@aol.com>
2022-07-08 12:02:40 -0300

    Ajuste para deploy
    Limpeza de arquivos desnecessários;
    Mais atualizações em breve

--- src/index.js
+++ src/index.js
@@ -6,15 +6,10 @@ import App from './App';
 import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/tp-trivia">
     <Provider store={ store }>
       <App />
     </Provider>
   </BrowserRouter>,
-
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
